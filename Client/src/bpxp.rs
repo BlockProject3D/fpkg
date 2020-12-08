0244d78562a1183614f9f09d6301bb51b64da917
Yuri Edward <yuri6037@outlook.com>
2020-12-08 21:15:26 +0100

    Added string section decoder

--- Client/src/bpxp.rs
+++ Client/src/bpxp.rs
@@ -33,8 +33,11 @@ use std::fs::File;
 use std::path::Path;
 use std::vec::Vec;
 use std::io;
+use std::io::Read;
+use std::collections::HashMap;
 use byteorder::LittleEndian;
 use byteorder::ByteOrder;
+use xz::stream::Stream;
 
 use crate::garraylen::*;
 
@@ -44,6 +47,7 @@ fn extract_slice<TArray: GenericArrayLen>(large_buf: &[u8], offset: usize) -> TA
     return TArray::from_array(buf);
 }
 
+#[derive(Copy, Clone)]
 struct BPXPMainHeader
 {
     signature: [u8;3], //+0
@@ -81,6 +85,7 @@ impl BPXPMainHeader
     }
 }
 
+#[derive(Copy, Clone)]
 struct BPXSectionHeader
 {
     pointer: u64, //+0
@@ -128,6 +133,42 @@ pub struct Decoder
     file: File
 }
 
+fn inflate_section(data: Vec<u8>, inflated_size: usize) -> io::Result<Vec<u8>>
+{
+    let mut unpacked: Vec<u8> = Vec::with_capacity(inflated_size);
+    let mut decoder = match Stream::new_stream_decoder(inflated_size as u64, xz::stream::TELL_NO_CHECK)
+    {
+        Err(e) => return Err(io::Error::new(io::ErrorKind::InvalidData, format!("[BPX] inflate error: {}", e))),
+        Ok(v) => v
+    };
+    let mut status = xz::stream::Status::MemNeeded;
+
+    while status != xz::stream::Status::StreamEnd
+    {
+        match decoder.process_vec(&data, &mut unpacked, xz::stream::Action::Finish)
+        {
+            Ok(s) => status = s,
+            Err(e) => return Err(io::Error::new(io::ErrorKind::InvalidData, format!("[BPX] inflate error: {}", e)))
+        }
+    }
+    return Ok(unpacked);
+}
+
+fn run_checksum(data: &[u8], expected_checksum: u32) -> io::Result<()>
+{
+    let mut chk: u32 = 0;
+
+    for i in 0..data.len()
+    {
+        chk += data[i] as u32;
+    }
+    if chk != expected_checksum
+    {
+        return Err(io::Error::new(io::ErrorKind::InvalidData, "[BPX] checksum validation failed"));
+    }
+    return Ok(());
+}
+
 impl Decoder
 {
     fn read_section_header_table(&mut self, checksum: u32) -> io::Result<()>
@@ -139,11 +180,11 @@ impl Decoder
             let (checksum, header) = BPXSectionHeader::read(&mut self.file)?;
             if header.flags & FLAG_COMPRESS_ZLIB == FLAG_COMPRESS_ZLIB
             {
-                return Err(io::Error::new(io::ErrorKind::InvalidInput, "[BPX] Zlib compression is not supported by FPKG"));
+                return Err(io::Error::new(io::ErrorKind::InvalidInput, "[BPX] zlib compression is not supported by FPKG"));
             }
             if header.flags & FLAG_CHECK_ADDLER32 == FLAG_CHECK_ADDLER32
             {
-                return Err(io::Error::new(io::ErrorKind::InvalidInput, "[BPX] Addler32 checksum is not supported by FPKG"));
+                return Err(io::Error::new(io::ErrorKind::InvalidInput, "[BPX] addler32 checksum is not supported by FPKG"));
             }
             final_checksum += checksum;
             self.sections.push(header);
@@ -155,12 +196,75 @@ impl Decoder
         return Ok(());
     }
 
+    fn find_section_by_type(&self, btype: u8) -> Option<BPXSectionHeader>
+    {
+        for v in &self.sections
+        {
+            if v.btype == btype
+            {
+                return Some(*v);
+            }
+        }
+        return None;
+    }
+
+    fn load_section(&mut self, section: &BPXSectionHeader) -> io::Result<Vec<u8>>
+    {
+        let mut data: Vec<u8> = Vec::with_capacity(section.csize as usize);
+
+        self.file.read(&mut data)?;
+        if section.flags & FLAG_COMPRESS_XZ == FLAG_COMPRESS_XZ
+        {
+            data = inflate_section(data, section.size as usize)?;
+        }
+        if section.flags & FLAG_CHECK_WEAK == FLAG_CHECK_WEAK
+        {
+            run_checksum(&data, section.chksum)?;
+        }
+        return Ok(data);
+    }
+
+    fn load_string_section(&mut self) -> io::Result<HashMap<u32, String>>
+    {
+        if let Some(section) = self.find_section_by_type(255)
+        {
+            let mut map: HashMap<u32, String> = HashMap::new();
+            let data = self.load_section(&section)?;
+            let mut ptr: u32 = 0;
+            let mut curs: Vec<u8> = Vec::new();
+
+            for i in 0..section.size
+            {
+                if data[i as usize] != 0x0
+                {
+                    curs.push(data[i as usize]);
+                }
+                else
+                {
+                    match String::from_utf8(curs)
+                    {
+                        Ok(v) =>
+                        {
+                            map.insert(ptr, v);
+                        },
+                        Err(e) => return Err(io::Error::new(io::ErrorKind::InvalidInput, format!("[BPX] error loading utf8 string: {}", e)))
+                    };
+                    curs = Vec::new();
+                    ptr = i + 1;
+                }
+            }
+            return Ok(map);
+        }
+        return Err(io::Error::new(io::ErrorKind::InvalidInput, "[BPX] could not locate string section"));
+    }
+
     pub fn new(file: &Path) -> io::Result<Decoder>
     {
         let mut fle = File::open(file)?;
         let (checksum, header) = BPXPMainHeader::read(&mut fle)?;
         let num = header.section_num;
-        let mut decoder = Decoder {
+        let mut decoder = Decoder
+        {
             file: fle,
             main_header: header,
             sections: Vec::with_capacity(num as usize)
@@ -170,7 +274,10 @@ impl Decoder
         return Ok(decoder);
     }
 
-    
+    pub fn extract_content(&mut self, target: &Path)
+    {
+        
+    }
 }
 
 pub struct Encoder
